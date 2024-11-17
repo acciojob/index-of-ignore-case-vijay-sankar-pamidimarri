433057cdecfe56125977abe8368bcbde8318ae90
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-17 08:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+    s1 = s1.toLowerCase();
+  s2 = s2.toLowerCase();
+
+  if (s2.length === 0 || s1.length === 0) {
+    return -1;
+  }
+
+  let to = s2.length;
+  for (let from = 0; from <= s1.length - s2.length; from++) {
+    if (s1.substring(from, from + to) === s2) {
+      return from;
+    }
+  }
+
+  return -1;
+	
 }
 
 // Please do not change the code below
